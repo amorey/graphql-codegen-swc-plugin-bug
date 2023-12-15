01c9e8c33eb643729a8a8a2abdb5361edf14a44b
Andres Morey <andresmarcel@gmail.com>
2023-12-15 10:53:53 +0300

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      plugins: [
+        ['@victorandree/graphql-codegen-client-preset-swc-plugin', { artifactDirectory: './src/gql', gqlTagName: 'graphql' }],
+      ],
+    }),
     splitVendorChunkPlugin(),
   ],
 })
